bce9eacf0df69803e9746df6a48ffd264c65fd6b
Олександр Холод <workkholod@gmail.com>
2025-05-25 23:41:18 +0300

    Axios
    Minor changes to new functionality.

--- src/http/commentAPI.js
+++ src/http/commentAPI.js
@@ -2,49 +2,44 @@ import { $host, $authhost } from "./index";
 
 export const comments = async function (
   page = 1,
-  pageSize = 5,
+  pageSize = 25,
   sortBy = "createdAt",
   sortOrder = "asc"
 ) {
-  // Логування параметрів
-  //console.log({ page, pageSize, sortBy, sortOrder });
-
   // Виправлений запит з правильними параметрами
   const { data } = await $host.get("api/comment", {
     params: { page, pageSize, sortBy, sortOrder },
   });
-  //console.log("data",data);
-  
+
   // Повертаємо отримані дані
   return data;
 };
 
-export const replies = async function (id, page = 1,pageSize = 5) {
- //console.log("lalala"); 
+export const replies = async function (id, page = 1, pageSize = 25) {
   const { data } = await $host.get(`api/comment/${id}/replies`, {
-    params: {id, page, pageSize },
+    params: { id, page, pageSize },
   });
-  //console.log("Відповіді з сервера:", data);
+  console.log("Response from server:", data);
   return data;
 };
-/*export const comments = async function () {
-  const { data } = await $host.get("api/comment");
-  return data;
-};*/
 
 // Метод для додавання коментаря
 export const addComment = async (commentData) => {
   const token = localStorage.getItem("token");
-
   // Логування всіх полів formData
+  /*
   for (let [key, value] of commentData.entries()) {
     console.log(key, value);
   }
-
+    */
+  const captchaId = commentData.get("captchaId");
+  const captchaInput = commentData.get("captchaInput");
   try {
     let response;
     const headers = {
       "Content-Type": "multipart/form-data",
+      "captcha-id": captchaId || "",
+      "captcha-input": captchaInput || "",
       Authorization: token ? `Bearer ${token}` : "", // Додаємо токен аутентифікації
     };
 
@@ -63,7 +58,7 @@ export const addComment = async (commentData) => {
     }
   } catch (error) {
     console.error("Error while adding comment:", error);
-    
+
     if (error.response && error.response.status === 403) {
       // Якщо сервер повернув 403, це може означати, що CAPTCHA не пройдена або токен недійсний
       throw new Error("Please complete the CAPTCHA first.");
