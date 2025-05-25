92f37b8860184f60fefc0f5761e07cda57442e91
Олександр Холод <workkholod@gmail.com>
2025-05-25 23:39:34 +0300

    Navbar fix
    Minor fix and changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,29 @@
 import React, { useContext, useState } from "react";
-import { ctx } from "../store/Context";
+import { ctx } from "../stores/Context";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Tooltip from "@mui/material/Tooltip";
 import Avatar from "@mui/material/Avatar";
 import Paper from "@mui/material/Paper";
-import { LOGIN_ROUTE, USER_ROUTE, COMMENTS_ROUTE } from "../utils/consts";
+import { LOGIN_ROUTE, COMMENTS_ROUTE } from "../utils/consts";
 import { observer } from "mobx-react-lite";
 import { useNavigate } from "react-router-dom";
 import { useTheme } from "../styles/theme-context";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
+import { Button } from "@mui/material";
+
 const { REACT_APP_API_URL } = require("../utils/consts");
 const NavBar = observer(() => {
-  const { user } = useContext(ctx);  
+  const { user } = useContext(ctx);
   const { toggleTheme, theme } = useTheme();
   const [anchorEl, setAnchorEl] = useState(null);
-  const handleMenu = (event) => {    
+
+  const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
@@ -37,27 +39,75 @@ const NavBar = observer(() => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>          
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, ml: 1 }}>
+      <AppBar position="static" color="primary">
+        <Box
+          sx={{
+            display: "flex",
+            position: "relative",
+            justifyContent: "space-between",
+           
+          }}
+        >
+          <Button
+            color="inherit"
+            sx={{
+              m:0,
+              textTransform: "none",
+              fontSize: "1.25rem",
+              borderRadius:0,
+              "&:hover": {
+                backgroundColor: "transparent", // без зміни фону
+                boxShadow: "none", // без тіні
+                border: "none", // без межі
+              },
+            }}
+            onClick={() => history(COMMENTS_ROUTE)}
+          >
             Comments
-          </Typography>
+          </Button>
 
-          <Tooltip title="Switch Theme">
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              onClick={toggleTheme}
-            >
-              {theme === "light" ? <Brightness7Icon /> : <Brightness4Icon />}
-            </IconButton>
-          </Tooltip>
-          <Box>
-            {user.isAuth ? (
-              <IconButton onClick={handleMenu} sx={{ p: 0 }}>
-                <Tooltip title={user._user.firstName || "Open settings"}>
-                  <Box>
+          <Toolbar>
+            <Tooltip title="Switch Theme">
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                onClick={toggleTheme}
+                sx={{ m: 0.5}}
+              >
+                {theme === "light" ? <Brightness7Icon /> : <Brightness4Icon />}
+              </IconButton>
+            </Tooltip>
+            <Box>
+              {user.isAuth ? (
+                <IconButton onClick={handleMenu} sx={{ p: 0 }}>
+                  <Tooltip title={user._user.firstName || "Open settings"}>
+                    <Box>
+                      <Paper
+                        sx={{
+                          display: "flex",
+                          alignContent: "center",
+                          justifyContent: "center",
+                          p: 0.4,
+                          borderRadius: "50%",
+                        }}
+                      >
+                        <Avatar
+                          sx={{ width: 48, height: 48 }}
+                          alt={user._user.userName || "User Avatar"}
+                          src={
+                            user._user.avatar
+                              ? `${REACT_APP_API_URL}uploads/avatars/${user._user.avatar}`
+                              : ""
+                          }
+                        />
+                      </Paper>
+                    </Box>
+                  </Tooltip>
+                </IconButton>
+              ) : (
+                <IconButton onClick={() => history(LOGIN_ROUTE)} sx={{ p: 0 }}>
+                  <Tooltip title="Account">
                     <Paper
                       sx={{
                         display: "flex",
@@ -67,79 +117,38 @@ const NavBar = observer(() => {
                         borderRadius: "50%",
                       }}
                     >
-                      <Avatar
-                        sx={{ width: 48, height: 48 }}
-                        alt={user._user.userName || "User Avatar"}
-                        src={
-                          user._user.avatar
-                            ? `${REACT_APP_API_URL}uploads/avatars/${user._user.avatar}`
-                            : ""
-                        }
-                      />
+                      <Avatar sx={{ width: 48, height: 48 }} />
                     </Paper>
-                  </Box>
-                </Tooltip>
-              </IconButton>
-            ) : (
-              <IconButton onClick={() => history(LOGIN_ROUTE)} sx={{ p: 0 }}>
-                <Tooltip title="Account">
-                  <Paper
-                    sx={{
-                      display: "flex",
-                      alignContent: "center",
-                      justifyContent: "center",
-                      p: 0.4,
-                      borderRadius: "50%",
-                    }}
-                  >
-                    <Avatar sx={{ width: 48, height: 48 }} />
-                  </Paper>
-                </Tooltip>
-              </IconButton>
-            )}
+                  </Tooltip>
+                </IconButton>
+              )}
 
-            {user.isAuth ? (
-              <>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-                >
-                  <MenuItem
-                    onClick={() => {
-                      handleClose();
-                      history(USER_ROUTE);
+              {user.isAuth ? (
+                <>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "right",
                     }}
-                  >
-                    Profile
-                  </MenuItem>
-                  <MenuItem
-                    onClick={() => {
-                      handleClose();
-                      history(COMMENTS_ROUTE);
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
                     }}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
                   >
-                    Store
-                  </MenuItem>                  
-                  <MenuItem onClick={handleClose}>Setting</MenuItem>
-                  <MenuItem onClick={() => logOut()}>Exit</MenuItem>
-                </Menu>
-              </>
-            ) : (
-              <></>
-            )}
-          </Box>
-        </Toolbar>
+                    <MenuItem onClick={() => logOut()}>Exit</MenuItem>
+                  </Menu>
+                </>
+              ) : (
+                <></>
+              )}
+            </Box>
+          </Toolbar>
+        </Box>
       </AppBar>
     </Box>
   );
